6f220bc151ec8e6da9622a83834f684fbacee936
Jose M. Coronado <j.coronado.personal@gmail.com>
2018-01-16 20:09:38 -0800

    [IMP] Removed attribute categories from the product.template since client thought it was confusing. Now the creation of the attribute_id for the sale order line is on the on_change of the product_id. This is more sustainable and it incorporated the logic that if there is 1 theme for the product, it will automatically apply it.

--- models/sale.py
+++ models/sale.py
@@ -9,6 +9,31 @@ class SaleOrderLine(models.Model):
     attribute_id = fields.Many2one('line.attribute', 'Custom Attributes',copy=False, index=True)
     show_full_circle = fields.Boolean(string='Show Full (Technical)',compute='_show_full_circle', readonly=True,store=False)
 
+    @api.constrains('product_id')
+    def create_attribute_id(self):
+        for line in self:
+            if line.attribute_id:
+                line.attribute_id.unlink()
+            new_attribute = line.env['line.attribute'].create({'order_line':line.id})
+            line.attribute_id = new_attribute
+            attribute_categories = None
+            if line.product_id.product_tmpl_id.attribute_theme_ids and len(line.product_id.product_tmpl_id.attribute_theme_ids) == 1:
+                if line.product_id.product_tmpl_id.attribute_theme_ids[0].attribute_values:
+                    for theme_line in line.product_id.product_tmpl_id.attribute_theme_ids[0].attribute_values:
+                        new_line = line.env['line.attribute.line'].create({
+                        'categ_id':theme_line.categ_id.id,
+                        'value':theme_line.value,
+                        'attribute_id':new_attribute.id,
+                        })
+            elif line.product_id.product_tmpl_id.categ_id.attribute_categ_ids:
+                attribute_categories = line.product_id.product_tmpl_id.categ_id.attribute_categ_ids
+            if attribute_categories:
+                for categ in attribute_categories:
+                    new_line = line.env['line.attribute.line'].create({
+                    'categ_id':categ.id,
+                    'attribute_id':new_attribute.id,
+                    })
+
     @api.multi
     def open_attribute_values(self):
         self.ensure_one()
